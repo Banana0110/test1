09ad1368e14a87d31356926f288602ebcf682839
Banana0110 <vip.smiles@list.ru>
2021-12-24 20:34:24 +0600

    =

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        for(int i = 1; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
